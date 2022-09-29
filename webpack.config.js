4e20d8329d2f9b88b98fb2725d44c797945915dc
Francesco Craighero <craighero.francesco@gmail.com>
2022-09-29 12:06:07 +0200

    Build site at 2022-09-29 10:06:07 UTC

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, '_webpack', 'main'),
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: '[name]-bundle.js',
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx'],
+    modules: ['node_modules'],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]'
+        }
+      },
+    ],
+  },
+};
