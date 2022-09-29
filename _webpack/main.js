ea2ba7183c3a31c785328c68f7347495d007ec98
Francesco Craighero <craighero.francesco@gmail.com>
2022-09-29 12:06:07 +0200

    Build site at 2022-09-29 10:06:07 UTC

---
+++ _webpack/main.js
@@ -0,0 +1,11 @@
+import './main.scss'
+import 'uikit/dist/js/uikit'
+
+// Import Fontawesome svgs
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin'
+import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons/faTwitterSquare'
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons/faGraduationCap'
+import { faPlaneDeparture } from '@fortawesome/free-solid-svg-icons/faPlaneDeparture'
+library.add(faLinkedin, faTwitterSquare, faGraduationCap, faPlaneDeparture)
+dom.watch()
